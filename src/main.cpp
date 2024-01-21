68fafc2a067a14ceb59555a39f0663bfc65f5ebe
AlicornLunaa <alicornlunaa@gmail.com>
2024-01-20 22:58:51 -0600

    Created SFML hello world

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,25 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
 int main(void){
+    RenderWindow window(VideoMode(640, 480), "Hello World!");
+    Event e;
+
+    while(window.isOpen()){
+        while(window.pollEvent(e)){
+            switch(e.type){
+                case Event::Closed:
+                    window.close();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+
     return 0;
 }
