b1a678c587bc508075c50116597e32033a224f44
AlicornLunaa <alicornlunaa@gmail.com>
2024-01-21 00:36:49 -0600

    Readme updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <reactphysics3d/reactphysics3d.h>
+#include <imgui.h>
+#include <imgui-SFML.h>
+using namespace std;
 using namespace sf;
 using namespace reactphysics3d;
 
@@ -8,10 +12,20 @@ int main(void){
     PhysicsWorld* world = physCommon.createPhysicsWorld();
 
     RenderWindow window(VideoMode(640, 480), "Hello World!");
-    Event e;
+    Clock deltaClock;
+    Time deltaTime;
+    
+    if(!ImGui::SFML::Init(window)){
+        cout << "Failure to initialize ImGUI\n";
+        return 1;
+    }
 
     while(window.isOpen()){
+        Event e;
+
         while(window.pollEvent(e)){
+            ImGui::SFML::ProcessEvent(window, e);
+
             switch(e.type){
                 case Event::Closed:
                     window.close();
@@ -22,9 +36,19 @@ int main(void){
             }
         }
 
+        deltaTime = deltaClock.restart();
+        ImGui::SFML::Update(window, deltaTime);
+
+        ImGui::Begin("Hello, world!");
+        if(ImGui::Button("Look at this pretty button"))
+            cout << "Button test\n";
+        ImGui::End();
+
         window.clear();
+        ImGui::SFML::Render(window);
         window.display();
     }
 
+    ImGui::SFML::Shutdown();
     return 0;
 }
